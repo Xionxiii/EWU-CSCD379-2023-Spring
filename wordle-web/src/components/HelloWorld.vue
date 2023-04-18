acbf39e8a3b4e948344922126f582cad63f334e3
Ryan Golden <65426263+Xionxiii@users.noreply.github.com>
2023-04-18 14:36:52 -0700

    update

--- wordle-web/src/components/HelloWorld.vue
+++ wordle-web/src/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="greetings">
-        <h1 class="green">{{ msg }}</h1>
-        <h3>
-            You’ve successfully created a project with
-            <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-            <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-        </h3>
-    </div>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Youï¿½ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
 </template>
 <script setup lang="ts">
-    defineProps<{
-        msg: string
-    }>()
+defineProps<{
+  msg: string
+}>()
 </script>
